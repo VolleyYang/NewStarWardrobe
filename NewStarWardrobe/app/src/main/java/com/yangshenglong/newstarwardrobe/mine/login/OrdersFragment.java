c76a8943a17bc3602b00023cb087a03bae114f1e
ChiSongTing <748233223@qq.com>
2017-01-04 17:39:16 +0800

    12312313

---
+++ NewStarWardrobe/app/src/main/java/com/yangshenglong/newstarwardrobe/mine/login/OrdersFragment.java
@@ -0,0 +1,29 @@
+package com.yangshenglong.newstarwardrobe.mine.login;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.yangshenglong.newstarwardrobe.R;
+import com.yangshenglong.newstarwardrobe.base.BaseFragment;
+
+/**
+ * Created by CST on 17/1/4.
+ */
+
+public class OrdersFragment extends BaseFragment{
+    private TextView tv;
+    @Override
+    public int setLayout() {
+        return R.layout.fragment_orders;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
